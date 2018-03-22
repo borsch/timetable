cc60a2f73176c120bd609edb694e93a981ceeb47
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2018-03-22 22:16:17 +0200

    refactoring

--- src/app/index.js
+++ src/app/index.js
@@ -5,48 +5,62 @@ const {ipcRenderer} = require('electron');
 const TimetableParser = require('../lib/timetable_parser');
 const timetableParser = new TimetableParser();
 
-angular.module('timeTableApp', [tabs])
-  .controller('mainCtrl', ['$scope', '$http', ($scope, $http) => {
+angular.module('timeTableApp', [tabs]).controller('mainCtrl', ['$scope', '$http', ($scope, $http) => {
+    let selectedPath;
 
     $scope.appTitle = 'Awesome timetable app';
 
     $scope.selectFile = function () {
-      console.log('log');
-      ipcRenderer.send('open-file-dialog');
+        ipcRenderer.send('open-file-dialog');
     };
 
     $scope.saveFile = function () {
-      ipcRenderer.send('save-dialog');
+        if (selectedPath) {
+            ipcRenderer.send('save-dialog');
+        }
     };
 
-    let selectedPath;
-
     ipcRenderer.on('selected-file', function (event, paths) {
-      selectedPath = paths[0];
-      $scope.selectedFilePath = `You selected: ${selectedPath}`;
+        let valid = true;
+        selectedPath = paths[0];
+
+        if (!selectedPath.endsWith('.xlsx') && !selectedPath.endsWith('.xls')) {
+            valid = false;
+            selectedPath = null;
+        }
+
+        $scope.$apply(function(){
+            if (valid) {
+                $scope.selectedFilePath = `You selected: ${selectedPath}`;
+            } else {
+                $scope.selectedFilePath = 'Supported only Excel files';
+            }
+
+            $scope.selectedSavePath = '';
+        });
     });
 
     ipcRenderer.on('saved-file', function (event, path) {
-      if (!path) path = 'No path';
-      $scope.selectedSavePath = path ? `File will be saved to: ${path}` : 'Save path was not selected, please try again';
-      if (path) {
-        const json = timetableParser.process_timetable(selectedPath);
-        export_json_to_file(json, path);
-      }
+        $scope.$apply(function(){
+            $scope.selectedSavePath = path ? `File will be saved to: ${path}` : 'Save path was not selected, please try again';
+        });
+
+        if (path) {
+            const json = timetableParser.process_timetable(selectedPath);
+            export_json_to_file(json, path);
+        }
     });
 
-    //timetableParser.process_timetable(`${__dirname}/../../example.xlsx`);
-
     function export_json_to_file(data, path) {
-      const jsonfile = require('jsonfile');
+        const jsonfile = require('jsonfile');
 
-      jsonfile.writeFile(path, data, {spaces: 4}, function (err) {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log('result exported to file [export.json] in root directory');
-        }
-      })
+        jsonfile.writeFile(path, data, {spaces: 4}, function (err) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('result exported to file [export.json] in root directory');
+            }
+        })
     }
 
-  }]);
+}]);
