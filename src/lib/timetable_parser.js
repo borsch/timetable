b48690edd1d0bafd2909a7a2d38b4b688abd194f
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2018-04-03 04:30:46 +0300

    process classroom & weeks

--- src/lib/timetable_parser.js
+++ src/lib/timetable_parser.js
@@ -11,7 +11,24 @@ module.exports = class TimetableParser {
   process_timetable(file_path) {
     const file_type = this._get_file_type(file_path);
     const parser = this._get_parser(file_type);
-    return parser.parse(file_path);
+    let result = parser.parse(file_path);
+    let self = this;
+
+    result.then(function(schedule){
+      schedule.forEach(function(subject){
+        let classroom = subject.classroom,
+            parts = classroom.split('-');
+
+        subject.corps = parseInt(parts[0].trim());
+        if (parts[1])
+          subject.room = parseInt(parts[1].trim());
+
+        subject.weeks_bool = self._process_weeks(subject.weeks);
+      });
+
+    });
+
+    return result;
   }
 
   _get_file_type(file_path) {
@@ -27,5 +44,25 @@ module.exports = class TimetableParser {
         'docx': this.cocTimetableParser
     };
     return FILE_TYPE_PARSER_MAP[file_type];
+  }
+
+  _process_weeks(weeks) {
+    let result = [...Array(15)].map(() => 0),
+        parts = weeks.toString().split(/[,\.]/);
+
+      parts.forEach(function(part){
+          if (part.indexOf('-') > -1) {
+            part = part.replace('--', '-');
+            let bounds = part.split('-');
+
+            for (let i = parseInt(bounds[0]); i <= parseInt(bounds[1]); ++i) {
+              result[i - 1] = 1;
+            }
+          } else {
+            result[parseInt(part) - 1] = 1;
+          }
+      });
+
+      return result;
   }
 };
