61d9879e3debc4ff8343b6bcb0af6712147d42f9
Vitalii Moholivskyi <mogolivskiy@gmail.com>
2018-04-20 11:06:50 +0300

    Add form

--- src/app/index.js
+++ src/app/index.js
@@ -2,17 +2,25 @@
 
 const tabs = require('angular-ui-bootstrap/src/tabs');
 const {ipcRenderer} = require('electron');
+const _ = require('lodash');
+
 const TimetableParser = require('../lib/timetable_parser');
 const timetableParser = new TimetableParser();
+const TimetableExporter = require('../lib/timetable_exporter');
+const timetableExporter = new TimetableExporter();
+
 const FILE_TYPE_VALIDATION_REGEXP = /.*\.(xlsx|xls|doc|docx)$/;
 
 const DAYS = ['Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П`ятниця', 'П\'ятниця', 'Субота', 'Cубота', 'Неділя', 'Неділя '];
 const TIME = ['8:30-9:50', '10:00-11:20', '11:40-13:00', '13:30-14:50', '15:00-16:20', '15.00-16.30', '16:30-17:50', '18:00-19:20'];
 
-angular.module('timeTableApp', [tabs]).controller('mainCtrl', ['$scope', '$http', ($scope) => {
+const timeTableApp = angular.module('timeTableApp', [tabs, 'datatables']).controller('mainCtrl', ['$scope', '$http', ($scope) => {
   let selectedPath;
 
   $scope.appTitle = 'Парсер розкладу';
+  $scope.timetable = {};
+  $scope.conflictsInTimetable = false;
+  $scope.timetables = [];
   $scope.schedule = null;
   $scope.possibleClassesTime = TIME;
   $scope.possibleDay = DAYS;
@@ -37,7 +45,46 @@ angular.module('timeTableApp', [tabs]).controller('mainCtrl', ['$scope', '$http'
       $scope.selectedSavePath = null;
   };
 
+  $scope.addTimetable = function () {
+    if ($scope.ctrl.timetableFrom.$valid && $scope.schedule && !$scope.conflictsInTimetable) {
+      $scope.timetable.schedule = $scope.schedule;
+      $scope.timetables.push($scope.timetable);
+      //$scope.resetTimetableForm();
+    }
+  };
+
+  $scope.doesConflicts = function (subject) {
+    const sameSubjects = $scope.schedule.filter((another) => {
+      const sumOfWeeks = _.zipWith(another.weeks_bool, subject.weeks_bool, function(a, b) {
+        return a + b;
+      });
+      const doesWeeksOverlap = sumOfWeeks.filter(n => n > 1).length > 0;
+      return another.day === subject.day
+        && another.time === subject.time
+        && another.subject === subject.subject
+        && doesWeeksOverlap;
+    });
+    const doesConflicts = sameSubjects.length > 1;
+    if (doesConflicts) {
+      $scope.conflictsInTimetable = true;
+      return true;
+    }
+    return false;
+  };
+
+  $scope.resetTimetableForm = function () {
+    $scope.timetable = {};
+    $scope.conflictsInTimetable = false;
+    $scope.reset_state();
+  };
+
+  $scope.exportTimetables = function () {
+    ipcRenderer.send('save-dialog');
+  };
+
   ipcRenderer.on('selected-file', function (event, paths) {
+    $scope.conflictsInTimetable = false;
+
     let valid = true;
     selectedPath = paths[0];
 
@@ -88,9 +135,13 @@ angular.module('timeTableApp', [tabs]).controller('mainCtrl', ['$scope', '$http'
     });
 
     if (path) {
-      const json = timetableParser.process_timetable(selectedPath).then((json) => {
+      /*const json = timetableParser.process_timetable(selectedPath).then((json) => {
         export_json_to_file(json, path);
-      })
+      })*/
+      timetableExporter.timetablesToXls($scope.timetables, path).then((smth) => {
+        console.dir(smth)
+      });
+      //export_json_to_file($scope.timetables, );
     }
   });
 
@@ -114,3 +165,10 @@ angular.module('timeTableApp', [tabs]).controller('mainCtrl', ['$scope', '$http'
     return json;
   }
 }]);
+
+timeTableApp.controller('smallPreview', function (DTOptionsBuilder, DTColumnDefBuilder) {
+  var vm = this;
+  vm.dtOptions = DTOptionsBuilder.newOptions()
+    .withDisplayLength(2)
+    .withDOM('pitrfl');
+});
