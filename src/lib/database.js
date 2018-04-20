b4ab3482d280ea35119758d9a681402aaffbe3a4
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2018-04-20 12:56:57 +0300

    lesson number

--- src/lib/database.js
+++ src/lib/database.js
@@ -9,6 +9,8 @@ const Subject = model(db, 'subject');
 const Auditorium = model(db, 'auditorium');
 const Group = model(db, 'group');
 
+const TIME = ['8:30-9:50', '10:00-11:20', '11:40-13:00', '13:30-14:50', '15:00-16:20', '15.00-16.30', '16:30-17:50', '18:00-19:20'];
+
 module.exports.save_schedule = function(schedules){
     schedules.forEach(function(schedule){
         let faculty = {
@@ -62,7 +64,7 @@ module.exports.save_schedule = function(schedules){
 
                 let subject_promise = await new Promise(function(resolve){
                     Subject.save(
-                        {name: item.subject, weeks: item.weeks_bool, time: item.time, day: item.day},
+                        {name: item.subject, weeks: item.weeks_bool, time: item.time, day: item.day, lesson_number: TIME.indexOf(item.time) + 1},
                         (error, saved) => resolve(saved)
                     );
                 });
